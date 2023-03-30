423146b240de787ca06dcfe550f64bcb79bc175b
Imadeddinn <ouali.aimad.eddine@gmail.com>
2023-03-29 17:04:21 -0700

    A dream doesnt become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
@@ -8,16 +13,17 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+
 /*
-* write your line of code here...
-* Remember:
-* main - magic
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
+* Here, we are dereferencing the pointer `p` and adding an offset of 5 to it.
+* This means we are assigning the value 98 to the memory location that is 5 integers
+* away from the location pointed to by `p`.
+* Since we have not allocated any memory at this location, this results in undefined behavior.
+* However, in this specific case, it seems to be overwriting the value stored in `a[2]`.
 */
 *(p + 5) = 98;
-/* ...so that this prints 98\n */
+
 printf("a[2] = %d\n", a[2]);
+
 return (0);
+}
